0be89b5a2d0481d535bff522ff989c02a57eb1c2
Rocky Meza <rockymeza@gmail.com>
2010-04-04 18:55:05 -0600

    some more documentation

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -4,37 +4,46 @@ require 'lib/token'
 module Remus
 
   class Lexer
-    ##| abstract class
+    # Lexer that tokenizes the input
+    
+    # The string to be tokenized
     attr_accessor :string
     
     def self.convert( string )
       lexer = self.new( string )
       
       lexer.before
-      lexer.main
+      lexer.tokenize
       lexer.after
       
       return lexer.output
     end
     
+    
     def before
     end
     
-    def main
+    
+    def tokenize
       @output << Token.new( @string )
     end
     
+    
     def after
     end
+    
     
     def output
       return @output
     end
     
+    
     def initialize( string )
       @string = string
       @output = String.new
     end
+    
+    
   end
   
 end
