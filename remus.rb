222e426da575b3e87851b6b0d489b2da4f33df25
Rocky Meza <rockymeza@gmail.com>
2010-04-04 18:26:27 -0600

    updated syntax, it's super cool now

--- remus.rb
+++ remus.rb
@@ -7,6 +7,14 @@ module Remus
     
   end
   
+  def convert( string, language = :plain_text)
+    class_name = language.to_s.split('_').select {|w| w.capitalize! || w }.join('')
+
+    require 'lang/#{language}' unless Remus.const_defined?(class_name)
+    
+    Remus.const_get(class_name).send('convert', string)
+  end
+  module_function :convert
   
   
 end
