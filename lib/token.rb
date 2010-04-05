ebcacf1b3aeec1553e93e4b83feef03be9eeb137
Rocky Meza <rockymeza@gmail.com>
2010-04-04 18:43:35 -0600

    added documentation---let's keep this up

--- lib/token.rb
+++ lib/token.rb
@@ -1,8 +1,11 @@
 class Token < String
   
-  
+  # The type of token that this is
+  # e.g. :plain, :constant, :literal, :number, :keyword, etc.
   attr_reader :type
   
+  # A special would be relevant for something like an opening parentheses
+  # e.g. :opener, :closer
   attr_reader :special
   
   def initialize( text, type = :plain, special = nil )
--- remus.rb
+++ remus.rb
@@ -2,19 +2,22 @@ require 'lib/lexer'
 
 module Remus
   
-  class PlainText < Lexer
-    
-    
-  end
-  
   def convert( string, language = :plain_text)
+    # convert the language symbol to a class_name
+    # e.g. :plain_text => PlainText
     class_name = language.to_s.split('_').select {|w| w.capitalize! || w }.join('')
-
-    require 'lang/#{language}' unless Remus.const_defined?(class_name)
     
+    # require the language file unless the class_name is defined already
+    # aka PlainText
+    require 'lang/#{language}' unless Remus.const_defined?(class_name) 
+    
+    # call convert on the lexer.
     Remus.const_get(class_name).send('convert', string)
   end
   module_function :convert
   
-  
+  # An empty Lexer class, basically returns the string untouched
+  # This is the default Lexer.
+  class PlainText < Lexer
+  end
 end
