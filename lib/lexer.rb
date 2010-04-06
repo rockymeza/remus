1e22bc9dc651d34d4063290ca6a2fe26b57dda5e
Rocky Meza <rockymeza@gmail.com>
2010-04-05 22:47:03 -0600

    extracted html from lexer into lang/html.rb and updated example

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -28,38 +28,7 @@ module Remus
     
     
     def tokenize
-      if @opened
-        case
-          when scan( />/ ) || scan( /\/>/ )
-            @opened = false
-            return Token.new( matched, :identifier )
-          when scan( /\w+=/ )
-            return Token.new( matched, :attribute )
-          when scan( /".*?"/ )
-            return Token.new( matched, :string )
-        else
-          scan /\s/
-          return Token.new( matched, :nocolor )
-        end
-      else
-        case
-          when peek(1) == '<'
-            if scan /<!--.*?-->/
-              return Token.new( matched, :comment )
-            elsif scan /<\/\w+>/
-              return Token.new( matched, :identifier )
-            elsif scan /<\w+>/
-              return Token.new( matched, :identifier)
-            else
-              scan /<\w+/
-              @opened = true
-              return Token.new( matched, :identifier )
-            end
-        else
-          scan /[^<]+/
-          return Token.new( matched )
-        end
-      end
+      return Token.new( getch )
     end
     
     
