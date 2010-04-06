9897778976eb03bcbebb261e45533d8343214345
Rocky <rocky@fusionbox.com>
2010-04-05 22:08:53 -0600

    added terminal color support

--- lib/token.rb
+++ lib/token.rb
@@ -17,12 +17,24 @@ class Token < String
   def wrap
     case type
       when :string
-        '<span class="string">' + @text + '</span>'
+        green( @text )
       when :tag
-        '<span class="tag">' + @text + '</span>'
+        red( @text )
+      when :attribute
+        blue( @text )
     else
       @text
     end
   end
   
+  def colorize(text, color_code)
+    "#{color_code}#{text}\e[0m"
+  end
+
+  def red(text); colorize(text, "\e[31m"); end
+  def green(text); colorize(text, "\e[32m"); end
+  def yellow(text); colorize(text, "\e[33m"); end
+  def blue(text); colorize(text, "\e[34m"); end
+  def purple(text); colorize(text, "\e[35m"); end
+  def cyan(text); colorize(text, "\e[36m"); end
 end
