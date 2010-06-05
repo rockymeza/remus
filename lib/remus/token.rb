60f7f49277edf049b9c5825986aac5acbc27915e
Rocky Meza <rockymeza@gmail.com>
2010-06-05 10:41:53 -0600

    more documentation

--- lib/remus/token.rb
+++ lib/remus/token.rb
@@ -7,18 +7,18 @@ module Remus::Token
   # A Token class should provide support for all of the
   # following types:
   # 
-  # 1. plain -- the Hello World in +<span>Hello World</span>+ for HTML
+  # 1. plain -- the <tt>Hello World</tt> in <tt><span>Hello World</span></tt> for HTML
   # 2. keyword -- +if+, +else+, +elsif+, +end+, +class+, +module+ in Ruby
   # 3. reserved -- +true+, +false+ in JavaScript
-  # 4. comment -- +/* comment */+ in CSS
-  # 5. string -- +"Hello World!"+ in PHP
+  # 4. comment -- <tt>/* comment */</tt> in CSS
+  # 5. string -- <tt>"Hello World!"</tt> in PHP
   # 6. number -- +4+ in Ruby
-  # 7. function -- +strip_tags()+ in PHP
-  # 8. property -- the +.length+ in +"Hello World".length+ in Javascript
-  # 9. operator -- +++, +-+, +&&+ in Ruby
-  # 10. identifier -- +<p>+ in HTML
-  # 11. variable -- +$var+ in PHP
-  # 12. punctuation -- nonoperator punctuation marks such as +;+ in PHP
+  # 7. function -- <tt>strip_tags()</tt> in PHP
+  # 8. property -- the +.length+ in <tt>"Hello World".length</tt> in Javascript
+  # 9. operator -- <tt>+</tt>, +-+, <tt>&&</tt> in Ruby
+  # 10. identifier -- <tt><p></tt> in HTML
+  # 11. variable -- <tt>$var</tt> in PHP
+  # 12. punctuation -- nonoperator punctuation marks such as <tt>;</tt> in PHP
   class Token < String
 
     # The type of the token
